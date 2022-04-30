178a9b624b8a19b32966ff9992af84423c59f15e
this_is_ash <hodoa817@gmail.com>
2022-04-30 11:14:18 +0000

    babel error

--- src/mobx/experienceStore.js
+++ src/mobx/experienceStore.js
@@ -1,19 +1,48 @@
-import cuid from "cuid";
+// import cuid from "cuid";
+import { observable, action, computed } from "mobx";
 
-export function createExperienceStore() {
-  return {
-    experiences: [],
-    addExperience(exp) {
-      this.experiences.push({
-        exp,
-        id: cuid(),
-      });
-      //add to db here or on top
+// export function createExperienceStore() {
+//   return {
+//     experiences: [],
+//     addExperience(exp) {
+//       this.experiences.push({
+//         exp,
+//         id: cuid(),
+//       });
+//       //add to db here or on top
+//     },
+//     removeExprience(id) {
+//       this.experiences = this.experiences.filter(
+//         (experiences) => experiences.id != id
+//       );
+//     },
+//   };
+// }
+
+class expStore {
+  @observable experiences = [
+    {
+      year: 1,
+      category: 2,
+      discription: "Winner: Student Excellence Awards from MobX",
+      hours: "40",
+      refName: "Lebene Gbedawo",
+      refContact: "lebene.bed@ashesi.edu.gh",
+      isHighlight: 0,
+      id: 1,
     },
-    removeExprience(id) {
-      this.experiences = this.experiences.filter(
-        (experiences) => experiences.id != id
-      );
+    {
+      year: 2,
+      category: 2,
+      discription: "Head of Communications Ashesi Start-up Launchpad at MobX",
+      hours: "40",
+      refName: "Lebene Gbedawo",
+      refContact: "lebene.bed@ashesi.edu.gh",
+      isHighlight: 0,
+      id: 2,
     },
-  };
+  ];
 }
+
+const store = new expStore();
+export default store;
